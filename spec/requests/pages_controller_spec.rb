be8d31d290d955ec630f1673d96a426b9f475e3e
PenPeen <hiroto@makkunoMacBook-Air.local>
2022-10-03 11:00:31 +0900

    微修正

---
+++ spec/requests/pages_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :request do
+  describe "#home" do
+    let(:title) {"トップページ（仮）"}
+
+    it "記事一覧ページを表示する" do
+      get '/pages/home'
+      expect(response).to have_http_status 200
+    end
+
+    it "トップページの文字列が表示される" do
+      get "/pages/home"
+      expect(response.body).to include("<p>#{title}</p>")
+    end
+  end
+end
