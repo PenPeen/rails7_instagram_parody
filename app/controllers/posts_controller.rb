722269a5285261d495cde33e86ce21f3e8a5c5b0
PenPeen <hiroto@makkunoMacBook-Air.local>
2022-10-04 22:39:51 +0900

    不具合修正。リファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,19 +9,16 @@ class PostsController < ApplicationController
     def create
         # エラーメッセージ表示用
         @post = Post.new(post_params)
-        @photos = @post.photos.build
 
         # validationエラー及び、画像が存在しているか。
-        if @post.photos.present?
-            if @post.save
-                redirect_to root_path, flash: {notice: '投稿が保存されました'}
-            end
+        # if @post.save && @post.photos.present?
+        if @post.save
+            redirect_to root_path, flash: {notice: '投稿が保存されました'}
+        else
+            @post.valid?
+            flash[:alert] = "投稿に失敗しました"
+            render :new, status: :unprocessable_entity
         end
-
-        @post.valid?
-        flash[:alert] = "投稿に失敗しました"
-        render :new, status: :unprocessable_entity
-        
     end
 
     private
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
     accepts_nested_attributes_for :photos
 
     validates :caption, presence: true
+
+    # photos（子レコードのバリデーションも有効とする）
+    validates_associated :photos
+    validates :photos, presence: true
 end
