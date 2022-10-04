af75952f7d37bc93d29fcb46a1e9f748c5601131
PenPeen <hiroto@makkunoMacBook-Air.local>
2022-10-04 14:51:14 +0900

    Photoモデルの作成。アソシエーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_054355) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_054821) do
+  create_table "photos", charset: "utf8mb4", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_photos_on_post_id"
+  end
+
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "caption"
     t.bigint "user_id", null: false
@@ -33,5 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_054355) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
 end
