e11da415ff2c9e6b24c6202b782f07e6301c2214
PenPeen <hiroto@makkunoMacBook-Air.local>
2022-10-27 13:57:44 +0900

    CarrierWaveバグ修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,9 @@ class Photo < ApplicationRecord
     belongs_to :post
     # validates :image, presence: true
 
-    # アップロードツールを「Carrier Wave」から、「ActiveStorage」に変更
-    has_one_attached :image
-    # mount_uploader :image, ImageUploader    #アップローダー
+    # アップロードツールを「Carrier Wave」
+    mount_uploader :image, ImageUploader    #アップローダー
+
+    # ActiveStorage Formオブジェクトでは利用できなかった。。。
+    # has_one_attached :image
 end
