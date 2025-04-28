a0f6860a0fca770575e2e28f426359ad91f3425c
Pen Peen <ikegami1022.h@gmail.com>
2025-04-28 21:09:52 +0900

    Docker環境をARM64プラットフォームに対応させ、MySQLのバージョンを8.0に更新。Bundlerのバージョンを固定し、SeleniumイメージをApple Silicon用に変更。データベーススキーマの照合順序をutf8mb4_0900_ai_ciに設定。

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
-FROM ruby:3.1
+FROM --platform=linux/arm64 ruby:3.1.2
 
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs mariadb-client
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs default-mysql-client
 RUN curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && apt-get install -y nodejs
 RUN npm install --global yarn
 RUN mkdir /myapp
 WORKDIR /myapp
 COPY Gemfile /myapp/Gemfile
 COPY Gemfile.lock /myapp/Gemfile.lock
-RUN gem install bundler
-RUN bundle install
+
+# Bundlerのバージョンを固定
+RUN gem install bundler -v 2.3.22
+RUN bundle _2.3.22_ install
+
 COPY . /myapp
 
 # Add a script to be executed every time the container starts.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_05_120608) do
-  create_table "likes", charset: "utf8mb4", force: :cascade do |t|
+  create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_120608) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "photos", charset: "utf8mb4", force: :cascade do |t|
+  create_table "photos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "image"
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_120608) do
     t.index ["post_id"], name: "index_photos_on_post_id"
   end
 
-  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "caption"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_120608) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
